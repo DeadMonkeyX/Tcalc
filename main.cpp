4eb950ba23353feb4fc794822f82b5a069fd244c
Dm0nK <i.deadmonkeyx@gmail.com>
2014-03-23 20:24:47 +0100

    Fixed colors for windows and Linux and apple.

--- main.cpp
+++ main.cpp
@@ -1,53 +1,88 @@
 #include <iostream>
-#include <string>
+#include <string.h>
 #include <sstream>
 #include <stdio.h>
 #include <stdlib.h>
 
-
 using namespace std;
 
-// Color support for Linux and Apple.
-#if defined(__APPLE__) || defined(__LINUX__)
+/*
+	Define color codes for Linux and Mac OS X
+*/
+
+#if defined(__APPLE__) || defined(__linux__)
 #define RESET   "\033[0m"
-#define BLACK   "\033[30m"      /* Black */
-#define RED     "\033[31m"      /* Red */
-#define GREEN   "\033[32m"      /* Green */
-#define YELLOW  "\033[33m"      /* Yellow */
-#define BLUE    "\033[34m"      /* Blue */
-#define MAGENTA "\033[35m"      /* Magenta */
-#define CYAN    "\033[36m"      /* Cyan */
-#define WHITE   "\033[37m"      /* White */
-#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
 #define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
-#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
-#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
-#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
-#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
 #define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
 #define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
 #endif
 
 
-// function to print the ascii logo because i dont wanna "re-create" it latere on!
+/*
+	Define color codes for windows, Please note !
+	This tool is not "Specially made" for Windows
+	So i have used the colors that almost match
+	the colors from Linux / Mac ! - DONT HATE!
+	OH YE AND WINDOWS REQUIRES A FUCKING LIBERY DEAL WITH IT !
+*/
+
+#if defined(_WIN32)
+#include <windows.h>
+#define RESET "0x07"  /* Reset to default color */
+#define BOLDRED "0x0C" /* Red */
+#define BOLDCYAN "0x0B" /* Cyan */
+#define BOLDWHITE "0x0F" /* White */
+
+/*
+	Because we simply cant "print" colors in windows commandprompt
+	we need to write a function to make our life easyere!
+*/
+
+
+
+	void winColor(string colorcode)
+	{
+        if(colorcode == "0x07")   SetConsoleTextAttribute( GetStdHandle( STD_OUTPUT_HANDLE ), 0x07 );
+        if(colorcode == "0x0C") SetConsoleTextAttribute( GetStdHandle( STD_OUTPUT_HANDLE ), 0x0C );
+        if(colorcode == "0x0B") SetConsoleTextAttribute( GetStdHandle( STD_OUTPUT_HANDLE), 0x0B );
+        if(colorcode == "0x0F") SetConsoleTextAttribute( GetStdHandle( STD_OUTPUT_HANDLE), 0x0F );
+	}
+
+#endif
+
+/*
+	Because i dont want to write my ascii logo more than once!
+	I'm lazy and making a function for printing it !
+*/
+
 void logo()
 {
-    	//enable colors for linux and apple
-	#if defined(__APPLE__) || defined(__LINUX__)
-    		cout << BOLDCYAN;
-	#endif
+   	// Now if we are running Linux or Mac OS X
+   	#if defined(__APPLE__) || defined(__linux__) 
+   		cout << BOLDCYAN;
+   	#endif
 
-    //print ascii file
+   	// Now if we are running Windows
+   	#if defined(_WIN32)
+   		winColor(BOLDCYAN);
+   	#endif
+
+   	// Lets Print the Ascii Logo !
     cout << " _____          _        _____  ___  " << endl;
     cout << "/__   \\___ __ _| | ___  |___ / / _ \\ " << endl;
     cout << "  / /\\/ __/ _` | |/ __|   |_ \\| | | |" << endl;
     cout << " / / | (_| (_| | | (__   ___) | |_| |" << endl;
     cout << " \\/   \\___\\__,_|_|\\___| |____(_)___/ " << endl;
 	cout << "====================================" << endl;
-	
-	//reset color to default for linux and apple
-	#if defined(__APPLE__) || defined(__LINUX__)
-	cout << RESET;
+
+	//Reset color for Linux and Mac OS X
+	#if defined(__APPLE__) || defined(__linux__)
+		cout << RESET;
+	#endif
+
+	//Reset color for Windows
+	#if defined(_WIN32)
+		winColor(RESET);
 	#endif
 
 }
@@ -58,27 +93,40 @@ void usage(char *argv[], int argc)
 {
     //print the logo
     logo();
-   
+
     //if there is more parameters than "3"
     if(argc < 3)
-    {           
-                  
-		#if defined(__APPLE__) || defined(__LINUX__)
+    {
+
+    	// Set color to RED for OSX and Linux 
+		#if defined(__APPLE__) || defined(__linux__)
 			cout << BOLDRED;
  		#endif
 
-			cout << "E: Missing Arguments";
-		#if defined(__APPLE__) || defined(__LINUX__)
+		//Set Color to RED for Windows
+		#if defined(_WIN32)
+			winColor(BOLDRED);
+		#endif
+
+		// Print Error
+		cout << "E: Missing Arguments";
+		
+		// Reset color for Linux and OS X
+		#if defined(__APPLE__) || defined(__linux__)
 			cout << RESET;
 		#endif
-		
+
+		// Reset color for Windows
+		#if defined(_WIN32)
+			winColor(RESET);
+		#endif
+
 		cout << "\n" << endl;
-    
+
 	}
 
-    cout << "Usage: " << argv[0] << " <Speed in Mbit> <Size in GB> [overhead in procent]\n " << endl;
-       
-   // printf("\n===DM0nk's Tcalc 3.0 ===\n\nUsage: %s <Speed in Mbit> <Size in GB>\n", argv[0]);
+	// Print usage
+    cout << "Usage: " << argv[0] << " <Speed in Mbit> <Size in GB> [overhead]\n " << endl;
     exit(1);
 
 }
@@ -102,7 +150,7 @@ int main (int argc, char *argv[])
 	{
 		sscanf(argv[3], "%i", &overhead);
 	}
-	
+
 
     //get size in kb
     kb = t * 1024 * 1024;
@@ -157,14 +205,28 @@ int main (int argc, char *argv[])
     cout.precision(3);
 
 
-	#if defined(__APPLE__) || defined(__LINUX__)
+	#if defined(__APPLE__) || defined(__linux__)
     cout << BOLDWHITE <<  "Size: " << RESET << t << transferFormat << BOLDWHITE << "Speed: " << RESET << s << " MB/s" << BOLDWHITE << "\nOverhead: "<< RESET <<  overhead << "%\n\n" << BOLDWHITE << "ETA: " << RESET;
     #endif
-	
+
 	#if defined(_WIN32)
-	cout << "Size: " << t << transferFormat << "Speed: " << s << " MB/s\n\n" << "ETA: ";
+	winColor(BOLDWHITE);
+	cout << "Size: ";
+	winColor(RESET);
+	cout << t << transferFormat;
+	winColor(BOLDWHITE);
+	cout << "Speed: ";
+	winColor(RESET);
+	cout << s << " MB/s\n";
+	winColor(BOLDWHITE);
+	cout << "Overhead: ";
+	winColor(RESET);
+	cout << overhead << "%\n\n";
+	winColor(BOLDWHITE);
+	cout << "ETA: ";
+	winColor(RESET);
 	#endif
-	
+
 	if(years != 0)
     {
     cout << years << " Years - ";
